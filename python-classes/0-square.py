7aa8dbbdc9c461172418197d57a90d94d0e668a6
umid1234 <umidaso1357@gmail.com>
2025-11-23 18:28:01 +0000

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,9 +3,12 @@
 Module 0-square
 Defines a class Square that represents a square.
 """
+
+
 class Square:
     """
     Class Square that defines a square.
     """
+
     def __init__(self, size):
         self.__size = size
